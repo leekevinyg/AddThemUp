cd66d46221e7b539ae0b5d949ba4259bd0cfca25
leekevinyg <klee2010@gmail.com>
2016-02-16 21:50:10 -0800

    added jest __tests__ folder

--- src/components/DoneFrame.js
+++ src/components/DoneFrame.js
@@ -7,6 +7,7 @@ var DoneFrame = React.createClass({
     return (
       <div className="well text-center">
         <h2>{this.props.doneStatus}</h2>
+        <button className="btn btn-default" onClick={this.props.resetGame}>Play Again</button>
       </div>
     );
   }
