069b72850442906d5303a904459684db8a15551c
leekevinyg <klee2010@gmail.com>
2016-02-19 02:23:30 -0800

    updated description of subset sum problem

--- src/algorithms/NSumSolver.js
+++ src/algorithms/NSumSolver.js
@@ -1,7 +1,12 @@
 "use strict";
 
 /* the following function determines if any non zero n-element subset of
-the array sums to the target sum */
+the array sums to the target sum
+
+Problem description:
+https://en.wikipedia.org/wiki/Subset_sum_problem#Pseudo-polynomial_time_dynamic_programming_solution
+
+*/
 var NSumSolver = function(arrayOfPossibleNumbers, targetSum) {
 
   // stub
